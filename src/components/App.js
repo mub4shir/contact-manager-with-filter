4896a10a1ad7ed3256f497c6a18526df22c373ed
mubashir <mubashir11131719@gmail.com>
2021-10-20 23:41:27 +0530

    hash router

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { uuid } from 'uuidv4';
 import api from '../api/contacts';
 import './App.css';
